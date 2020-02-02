cb65f8953c9716f60b418cdc21d06360a91f99e4
KunlinY <kunliny@qq.com>
2020-02-02 03:19:19 -0800

    add logging

--- cnn/train_search_new.py
+++ cnn/train_search_new.py
@@ -4,8 +4,6 @@ import time
 import glob
 import numpy as np
 import torch
-import gc
-import psutil
 import utils
 import logging
 import argparse
@@ -15,7 +13,6 @@ import torch.nn.functional as F
 import torchvision.datasets as dset
 import torch.backends.cudnn as cudnn
 
-from torch.autograd import Variable
 from model_search import Network
 from architect import Architect
 
@@ -27,7 +24,7 @@ parser.add_argument('--learning_rate_min', type=float, default=0.001, help='min 
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
 parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
 parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
-parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
+parser.add_argument('--gpu', type=str, default='0', help='gpu device id, split with ","')
 parser.add_argument('--epochs', type=int, default=50, help='num of training epochs')
 parser.add_argument('--init_channels', type=int, default=16, help='num of init channels')
 parser.add_argument('--layers', type=int, default=8, help='total number of layers')
@@ -63,25 +60,40 @@ def main():
         sys.exit(1)
 
     np.random.seed(args.seed)
-    torch.cuda.set_device(args.gpu)
-    cudnn.benchmark = True
+    # torch.cuda.set_device(args.gpu)
+    gpus = [int(i) for i in args.gpu.split(',')]
+    if len(gpus) == 1:
+        torch.cuda.set_device(int(args.gpu))
+
+    # cudnn.benchmark = True
     torch.manual_seed(args.seed)
-    cudnn.enabled = True
+    # cudnn.enabled=True
     torch.cuda.manual_seed(args.seed)
-    logging.info('gpu device = %d' % args.gpu)
+    logging.info('gpu device = %s' % args.gpu)
     logging.info("args = %s", args)
 
     criterion = nn.CrossEntropyLoss()
     criterion = criterion.cuda()
     model = Network(args.init_channels, CIFAR_CLASSES, args.layers, criterion)
     model = model.cuda()
+    if len(gpus) > 1:
+        print("Parallel True")
+        model = nn.parallel.DataParallel(model, device_ids=gpus, output_device=gpus[0])
+        model = model.module
+
+    arch_params = list(map(id, model.arch_parameters()))
+    weight_params = filter(lambda p: id(p) not in arch_params,
+                           model.parameters())
+
     logging.info("param size = %fMB", utils.count_parameters_in_MB(model))
 
     optimizer = torch.optim.SGD(
-        model.parameters(),
+        # model.parameters(),
+        weight_params,
         args.learning_rate,
         momentum=args.momentum,
         weight_decay=args.weight_decay)
+    optimizer = nn.DataParallel(optimizer, device_ids=gpus)
 
     train_transform, valid_transform = utils._data_transforms_cifar10(args)
     train_data = dset.CIFAR10(root=args.data, train=True, download=True, transform=train_transform)
@@ -101,9 +113,9 @@ def main():
         pin_memory=True, num_workers=2)
 
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-        optimizer, float(args.epochs), eta_min=args.learning_rate_min)
+        optimizer.module, float(args.epochs), eta_min=args.learning_rate_min)
 
-    architect = Architect(model, args)
+    architect = Architect(model, criterion, args)
 
     for epoch in range(args.epochs):
         scheduler.step()
@@ -117,12 +129,12 @@ def main():
         print(F.softmax(model.alphas_reduce, dim=-1))
 
         # training
-        print('Train epoch number: ', len(train_queue))
         train_acc, train_obj = train(train_queue, valid_queue, model, architect, criterion, optimizer, lr)
         logging.info('train_acc %f', train_acc)
 
         # validation
-        valid_acc, valid_obj = infer(valid_queue, model, criterion)
+        with torch.no_grad():
+            valid_acc, valid_obj = infer(valid_queue, model, criterion)
         logging.info('valid_acc %f', valid_acc)
 
         utils.save(model, os.path.join(args.save, 'weights.pt'))
@@ -137,13 +149,13 @@ def train(train_queue, valid_queue, model, architect, criterion, optimizer, lr):
         model.train()
         n = input.size(0)
 
-        input = Variable(input, requires_grad=False).cuda()
-        target = Variable(target, requires_grad=False).cuda(non_blocking=True)
+        input = input.cuda()
+        target = target.cuda()
 
         # get a random minibatch from the search queue with replacement
         input_search, target_search = next(iter(valid_queue))
-        input_search = Variable(input_search, requires_grad=False).cuda()
-        target_search = Variable(target_search, requires_grad=False).cuda(non_blocking=True)
+        input_search = input_search.cuda()
+        target_search = target_search.cuda()
 
         architect.step(input, target, input_search, target_search, lr, optimizer, unrolled=args.unrolled)
 
@@ -152,19 +164,16 @@ def train(train_queue, valid_queue, model, architect, criterion, optimizer, lr):
         loss = criterion(logits, target)
 
         loss.backward()
-        nn.utils.clip_grad_norm(model.parameters(), args.grad_clip)
-        optimizer.step()
+        nn.utils.clip_grad_norm_(model.parameters(), args.grad_clip)
+        optimizer.module.step()
 
         prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
-        loss, prec1, prec5 = float(loss), float(prec1), float(prec5)
-        objs.update(loss, n)
-        top1.update(prec1, n)
-        top5.update(prec5, n)
+        objs.update(loss.item(), n)
+        top1.update(prec1.item(), n)
+        top5.update(prec5.item(), n)
 
         if step % args.report_freq == 0:
             logging.info('train %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
-            cpuStats()
-            logging.info("=================")
 
     return top1.avg, objs.avg
 
@@ -176,41 +185,22 @@ def infer(valid_queue, model, criterion):
     model.eval()
 
     for step, (input, target) in enumerate(valid_queue):
-        input = Variable(input, volatile=True).cuda()
-        target = Variable(target, volatile=True).cuda(non_blocking=True)
+        input = input.cuda()
+        target = target.cuda()
 
         logits = model(input)
         loss = criterion(logits, target)
 
         prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
         n = input.size(0)
-        loss, prec1, prec5 = float(loss), float(prec1), float(prec5)
-        objs.update(loss, n)
-        top1.update(prec1, n)
-        top5.update(prec5, n)
+        objs.update(loss.item(), n)
+        top1.update(prec1.item(), n)
+        top5.update(prec5.item(), n)
 
         if step % args.report_freq == 0:
             logging.info('valid %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
-            cpuStats()
-            logging.info("=================")
 
     return top1.avg, objs.avg
-
-
-def memReport():
-    for obj in gc.get_objects():
-        if torch.is_tensor(obj):
-            print(type(obj), obj.size())
-
-
-def cpuStats():
-    print(sys.version)
-    print(psutil.cpu_percent())
-    print(psutil.virtual_memory())  # physical memory usage
-    pid = os.getpid()
-    py = psutil.Process(pid)
-    memoryUse = py.memory_info()[0] / 2. ** 30  # memory use in GB...I think
-    print('memory GB:', memoryUse)
 
 
 if __name__ == '__main__':
---
+++ run_parallel.sh
@@ -0,0 +1,2 @@
+git pull
+python cnn/train_search_new.py --unrolled --report_freq 10 --gpu 0,1
