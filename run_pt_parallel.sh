0fb23657c25a7b26d74151a12aab05c2716c4db0
KunlinY <kunliny@qq.com>
2020-02-05 11:10:49 -0800

    fix shm

--- run_pt_parallel.sh
+++ run_pt_parallel.sh
@@ -1,6 +1,7 @@
 git pull
 cd pt.darts || exit
 pip install graphviz
+ipcs -lm
 python search.py --name cifar10-mg --dataset cifar10 --gpus all \
-    --batch_size 256 --workers 16 --print_freq 10 \
+    --batch_size 256 --workers 4 --print_freq 10 \
     --w_lr 0.1 --w_lr_min 0.004 --alpha_lr 0.0012
