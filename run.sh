db83d5db0656e3800c6223a9b400bc8d891bc0df
KunlinY <kunliny@qq.com>
2020-02-05 10:28:50 -0800

    change pip install

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 git pull
+pip install requirements.txt
 python cnn/train_search.py --unrolled --report_freq 10
--- run_pt.sh
+++ run_pt.sh
@@ -1,3 +1,4 @@
 git pull
 cd pt.darts || exit
+pip install requirements.txt
 python search.py --name cifar10 --dataset cifar10
