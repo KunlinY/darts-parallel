69258b0ee1e419f26d460819d74d7caada094bec
KunlinY <kunliny@qq.com>
2020-02-18 22:50:30 -0800

    fix gradient bugs

--- darts_slow/search.py
+++ darts_slow/search.py
@@ -149,7 +149,7 @@ def train(train_loader, valid_loader, model, architect, w_optim, alpha_optim, lr
         nn.utils.clip_grad_norm_(model.weights(), config.w_grad_clip)
 
         for param in model.parameters():
-            noise = (shape_gaussian[param.grad.shape].sample() / config.batch_size).to(param.grad.device)
+            noise = shape_gaussian[param.grad.shape].sample().to(param.grad.device)
             param.grad += noise
 
         w_optim.step()
