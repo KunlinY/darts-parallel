8d03fa2ee8d7f2adfdffe7e40ce3aff21cc46ffe
KunlinY <kunliny@qq.com>
2020-05-05 16:53:42 -0700

    modify big

--- darts_slow/run_darts_parallel_noise.sh
+++ darts_slow/run_darts_parallel_noise.sh
@@ -4,5 +4,5 @@ apt-get update
 apt-get --assume-yes install graphviz
 pip install graphviz pydot
 python search.py --name cifar10-mg --dataset cifar10 --gpus all \
-    --batch_size 128 --workers 16 --print_freq 1 \
+    --batch_size 128 --workers 8 --print_freq 1 \
     --w_lr 0.1 --w_lr_min 0.004 --alpha_lr 0.0012 --noise True
