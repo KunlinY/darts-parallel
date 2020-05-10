4da2b4f6054bd1845a2f234edb73a046fe189d1b
KunlinY <kunliny@qq.com>
2020-05-09 18:49:01 -0700

    change augment

--- darts_slow/augment.sh
+++ darts_slow/augment.sh
@@ -2,6 +2,5 @@ git pull
 apt-get update
 apt-get --assume-yes install graphviz
 pip install graphviz pydot
-python augment.py --name cifar10 --dataset cifar10 --gpus all \
-    --batch_size "$1" --workers "$2" --genotype "$3" \
-    --print_freq 10 --lr 0.1
+python augment.py --name cifar10 --dataset cifar10 \
+    --batch_size 64 --workers "$1" --genotype "$2"
