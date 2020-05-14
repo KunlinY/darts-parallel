3e3caae1abdfb7962edd77dc7811c8b755d74cf8
KunlinY <kunliny@qq.com>
2020-05-14 15:44:41 -0700

    try ps

--- darts_slow/run/process.py
+++ darts_slow/run/process.py
@@ -1,4 +1,4 @@
 if __name__ == "__main__":
-    for l in open("2_noise.txt", encoding="utf16"):
+    for l in open("1_noise.txt", encoding="utf8"):
         if "Valid" in l and "Final Prec@1 " in l:
             print(l[48:].strip())
