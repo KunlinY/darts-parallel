3568a8bcdea19c2088a283e87fd31075b55fe9c5
KunlinY <kunliny@qq.com>
2020-05-14 22:55:35 -0700

    try ps

--- ps/run_trainer.sh
+++ ps/run_trainer.sh
@@ -2,5 +2,5 @@ git pull
 apt-get update
 apt-get --assume-yes install graphviz ufw
 pip install graphviz pydot
-pip install --upgrade torch torchv
-isionpython rpc_parameter_server.py --world_size="$1" --rank="$2" --num_gpus=1
+pip install --upgrade torch torchvision
+python rpc_parameter_server.py --world_size="$1" --rank="$2" --num_gpus=1
