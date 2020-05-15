b964159fec5919a9c765b9309fb044126cae3423
KunlinY <kunliny@qq.com>
2020-05-14 18:50:33 -0700

    try ps

--- ps/run_server.sh
+++ ps/run_server.sh
@@ -2,4 +2,6 @@ git pull
 apt-get update
 apt-get --assume-yes install graphviz ufw
 pip install graphviz pydot
-python rpc_parameter_server.py ---world_size="$1" --rank=0 --num_gpus=1
+pip install --upgrade torch torchvision
+ufw allow 29500/tcp
+python rpc_parameter_server.py --world_size="$1" --rank=0 --num_gpus=1
--- ps/run_trainer.sh
+++ ps/run_trainer.sh
@@ -2,4 +2,5 @@ git pull
 apt-get update
 apt-get --assume-yes install graphviz ufw
 pip install graphviz pydot
-python rpc_parameter_server.py ---world_size="$1" --rank="$2" --num_gpus=1
+pip install --upgrade torch torchv
+isionpython rpc_parameter_server.py --world_size="$1" --rank="$2" --num_gpus=1
