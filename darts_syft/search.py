c5df6b0397c44d715a5e247e3331d43805a2c380
KunlinY <kunliny@qq.com>
2020-05-17 18:36:28 -0700

    pysyft

--- darts_syft/search.py
+++ darts_syft/search.py
@@ -161,8 +161,8 @@ def update(step, wid, model, alpha_optim, w_optim, architect, lr):
     for w in workers:
         print(w.id, 'has objects ', len(w._objects))
 
-    model_ptr = model.send(trn_X.location)
-    print(model_ptr)
+    print(model)
+    model = model.copy().send(trn_X.location)
     print(model)
     model = model.to(device)
 
